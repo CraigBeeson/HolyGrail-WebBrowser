61a7709a67d930f6d25da12fde6ca7c85eb6751b
Craig Beeson <71735305+CraigBeeson@users.noreply.github.com>
2024-05-03 16:50:18 -0700

    Delete plugins/Html Editor/dockEditor.py

--- plugins/Html Editor/dockEditor.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtWebEngineWidgets import *
-
-class EditorDock(QDockWidget):
-	def __init__(self):
-		super(EditorDock,self).__init__()
-		
